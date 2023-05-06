750c91899daebe46239c3828e01469d54fb1b06c
Brianna McNeil <brianna.c.mcneil@icloud.com>
2023-05-06 11:58:28 -0400

    added a view and piano component

---
+++ vue/src/views/PageView.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="title-container">
+    <h4>Welcome</h4>
+    <h1>Do-Re-Mi-Fa-So-La-Ti-Do</h1>
+  </div>
+  <div class="button-container">
+    <!-- Button Component -->
+  </div>
+  <div class="piano-container">
+    <PianoComponent />
+  </div>
+  <div class="response-container">
+    <!-- Response Component -->
+  </div>
+</template>
+
+<script setup lang="ts">
+
+import PianoComponent from "../components/PianoComponent.vue"
+
+</script>
+
+<style>
+
+div {
+  border: 1px solid black;
+}
+
+.title-container > h4, h1 {
+  height: 5vh;
+  width: 100%;
+}
+
+.button-container {
+  width: 20vw;
+  height: 7vh;
+}
+
+.piano-container {
+  width: 100%;
+  height: 45vh;
+}
+
+.response-container {
+  width: 100%;
+  height: 5vh;
+}
+
+
+</style>
